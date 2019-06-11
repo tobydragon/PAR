09fb8a755756a4ec1c58999ffa76fbe85cff842a
Toby Dragon <dr.toby.dragon@gmail.com>
2019-06-11 13:31:59 -0400

    added logging, made api respond with hardcoded json to question request

--- src/main/java/edu/ithaca/dragon/par/spring/ApiController.java
+++ src/main/java/edu/ithaca/dragon/par/spring/ApiController.java
@@ -1,13 +1,28 @@
 package edu.ithaca.dragon.par.spring;
 
+import edu.ithaca.dragon.par.ParServer;
+import edu.ithaca.dragon.par.domain.Question;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ApiController {
 
+    ParServer parServer;
+
+    ApiController(){
+        super();
+        parServer = new ParServer();
+    }
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "Greetings from PAR API!";
     }
+
+    @RequestMapping("/nextQuestion")
+    public Question nextQuestion() {
+        return parServer.nextQuestion();
+    }
+
 }
--- src/test/java/edu/ithaca/dragon/par/spring/ParApplicationTests.java
+++ src/test/java/edu/ithaca/dragon/par/spring/ParApplicationTests.java
@@ -28,7 +28,7 @@ public class ParApplicationTests {
 	public void getHelloTest() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Greetings from Spring Boot!")));
+				.andExpect(content().string(equalTo("Greetings from PAR API!")));
 	}
 
 }
