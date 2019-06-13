a4852cae50b9a88d6afda22b928cb44d1686b6d5
KandacePC <krichards1@ithaca.edu>
2019-06-13 12:03:24 -0400

    user response stuff added

--- src/main/java/edu/ithaca/dragon/par/studentModel/UserResponse.java
+++ src/main/java/edu/ithaca/dragon/par/studentModel/UserResponse.java
@@ -1,4 +1,37 @@
 package edu.ithaca.dragon.par.studentModel;
+import edu.ithaca.dragon.par.domain.Question;
 
 public class UserResponse {
+    private String questionId;
+    private String userId;
+    private String responseText;
+    private Question question;
+    // private boolean correct;
+//userId probably should only be in UserResponseSet
+//**makes sense because a new UserResponseSet should be generated with each student
+//or are the questions being asked for a specific userId?
+
+    public UserResponse(String userIdIn, Question questionIn,String responseIn){
+        this.userId=userIdIn;
+        this.question=questionIn;
+        this.questionId=questionIn.getId();
+        this.responseText=responseIn;
+    }
+
+    /**
+     * checks if user response is correct or not
+     * @return true or false
+     */
+    public boolean checkResponse(){
+        if(responseText==question.getCorrectAnswer()) return true;
+        else return false;
+    }
+
+    public String getUserId(){
+        return userId;
+    }
+    public String getQuestionId(){
+        return questionId;
+    }
 }
+
--- src/test/java/edu/ithaca/dragon/par/studentModel/UserResponseSetTest.java
+++ src/test/java/edu/ithaca/dragon/par/studentModel/UserResponseSetTest.java
@@ -1,4 +1,58 @@
 package edu.ithaca.dragon.par.studentModel;
+import edu.ithaca.dragon.par.domain.Question;
+
+import org.junit.Test;
+import java.util.Arrays;
+import static org.junit.Assert.assertEquals;
+
 
 public class UserResponseSetTest {
+
+    @Test
+    public void getResponseSizeTest() {
+        Question q1 = new Question("Test Q1", "What is your name?", 1, "King Arthur",
+                Arrays.asList("Bob","Cindy","Joey","King Arthur"));
+        UserResponse response1 = new UserResponse("kandace", q1,"hey");
+        UserResponseSet respSet=new UserResponseSet("kandace");
+        respSet.addResponse(response1);
+
+
+        Question q2 = new Question("Test Q2", "What is your quest?", 3, "To seek the Holy Grail",
+                Arrays.asList("To Graduate","To make an ITS","To seek the Holy Grail"));
+        UserResponse response2 = new UserResponse("kandace", q2,"hey");
+        respSet.addResponse(response2);
+
+
+        Question q3 = new Question("Test Q3","What is the airspeed velocity of an unladen swallow?", 10, "African or European?",
+                Arrays.asList("10 m/s", "20 m/s", "30 m/s","African or European?"));
+        UserResponse response3 = new UserResponse("kandace", q3,"hey");
+        respSet.addResponse(response3);
+        assertEquals(respSet.getUserResponsesSize(),3);
+    }
+
+
+    public static double OK_DOUBLE_MARGIN = (double) 0.00001;
+    @Test
+    public void getScoreTest(){
+
+        Question q1 = new Question("Test Q1", "What is your name?", 1, "King Arthur",
+                Arrays.asList("Bob","Cindy","Joey","King Arthur"));
+        UserResponse response1 = new UserResponse("kandace", q1,"King Arthur");
+        UserResponseSet respSet=new UserResponseSet("kandace");
+        respSet.addResponse(response1);
+
+
+        Question q2 = new Question("Test Q2", "What is your quest?", 3, "To seek the Holy Grail",
+                Arrays.asList("To Graduate","To make an ITS","To seek the Holy Grail"));
+        UserResponse response2 = new UserResponse("kandace", q2,"To seek the Holy Grail");
+        respSet.addResponse(response2);
+
+
+        Question q3 = new Question("Test Q3","What is the airspeed velocity of an unladen swallow?", 10, "African or European?",
+                Arrays.asList("10 m/s", "20 m/s", "30 m/s","African or European?"));
+        UserResponse response3 = new UserResponse("kandace", q3,"African r European?");
+        respSet.addResponse(response3);
+
+        assertEquals(66.6666666,respSet.getScore(),OK_DOUBLE_MARGIN);
+    }
 }
