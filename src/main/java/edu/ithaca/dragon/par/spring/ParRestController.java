d790faee3c56bb21bd70b4b28ca4d9b5eddf9f4f
Aine <m.g.firefox@gmail.com>
2019-06-14 10:20:09 -0400

    Spring working, pom updated

--- src/main/java/edu/ithaca/dragon/par/spring/ParRestController.java
+++ src/main/java/edu/ithaca/dragon/par/spring/ParRestController.java
@@ -3,11 +3,10 @@ package edu.ithaca.dragon.par.spring;
 import edu.ithaca.dragon.par.ParServer;
 import edu.ithaca.dragon.par.domain.Question;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
 public class ParRestController {
 
     ParServer parServer;
