724328cebc6814cbc1b1c45b2d188328a3380edc
KandacePC <krichards1@ithaca.edu>
2019-06-17 11:56:01 -0400

    changes to UserResponseSetTest

--- src/test/java/edu/ithaca/dragon/par/studentModel/UserResponseSetTest.java
+++ src/test/java/edu/ithaca/dragon/par/studentModel/UserResponseSetTest.java
@@ -1,11 +1,10 @@
 package edu.ithaca.dragon.par.studentModel;
 import edu.ithaca.dragon.par.domainModel.Question;
 
+import edu.ithaca.dragon.par.io.ImageTaskResponse;
 import edu.ithaca.dragon.util.JsonUtil;
 import org.junit.Test;
-
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -16,9 +15,10 @@ public class UserResponseSetTest {
     @Test
     public void getResponseSizeTest() throws IOException{
         List<Question> questionsFromFile = JsonUtil.listFromJsonFile("src/test/resources/author/SampleQuestions.json", Question.class);
-        UserResponseSet respSet=new UserResponseSet("kandace");
+        List<ImageTaskResponse> responsesFromFile = JsonUtil.listFromJsonFile("src/test/resources/author/SampleResponses.json", ImageTaskResponse.class);
+        UserResponseSet respSet=new UserResponseSet(responsesFromFile.get(0).getUserId());
         for(int i=0;i<questionsFromFile.size();i++){
-            UserResponse response = new UserResponse("kandace", questionsFromFile.get(i),questionsFromFile.get(i).getCorrectAnswer());
+            UserResponse response = new UserResponse(responsesFromFile.get(0).getUserId(), questionsFromFile.get(i),responsesFromFile.get(0).getResponseTexts().get(i));
             respSet.addResponse(response);
         }
         assertEquals(respSet.getUserResponsesSize(),3);
@@ -27,26 +27,23 @@ public class UserResponseSetTest {
 
     public static double OK_DOUBLE_MARGIN = (double) 0.00001;
     @Test
-    public void getScoreTest(){
+    public void getScoreTest() throws IOException{
+        //all correct-100
+        List<Question> questionsFromFile = JsonUtil.listFromJsonFile("src/test/resources/author/SampleQuestions.json", Question.class);
+        List<ImageTaskResponse> responsesFromFile = JsonUtil.listFromJsonFile("src/test/resources/author/SampleResponses.json", ImageTaskResponse.class);
+        UserResponseSet respSet=new UserResponseSet(responsesFromFile.get(0).getUserId());
+        for(int i=0;i<questionsFromFile.size();i++){
+            UserResponse response = new UserResponse(responsesFromFile.get(0).getUserId(), questionsFromFile.get(i),responsesFromFile.get(0).getResponseTexts().get(i));
+            respSet.addResponse(response);
+        }
+        assertEquals(100.0,respSet.CalcScore(),OK_DOUBLE_MARGIN);
 
-        Question q1 = new Question("Test Q1", "What is your name?", 1, "King Arthur",
-                Arrays.asList("Bob","Cindy","Joey","King Arthur"));
-        UserResponse response1 = new UserResponse("kandace", q1,"King Arthur");
-        UserResponseSet respSet=new UserResponseSet("kandace");
-        respSet.addResponse(response1);
-
-
-        Question q2 = new Question("Test Q2", "What is your quest?", 3, "To seek the Holy Grail",
-                Arrays.asList("To Graduate","To make an ITS","To seek the Holy Grail"));
-        UserResponse response2 = new UserResponse("kandace", q2,"To seek the Holy Grail");
-        respSet.addResponse(response2);
-
-
-        Question q3 = new Question("Test Q3","What is the airspeed velocity of an unladen swallow?", 10, "African or European?",
-                Arrays.asList("10 m/s", "20 m/s", "30 m/s","African or European?"));
-        UserResponse response3 = new UserResponse("kandace", q3,"African r European?");
-        respSet.addResponse(response3);
-
-        assertEquals(66.6666666,respSet.CalcScore(),OK_DOUBLE_MARGIN);
+        //only gets one right-33.33
+        UserResponseSet respSet2=new UserResponseSet(responsesFromFile.get(0).getUserId());
+        for(int i=0;i<questionsFromFile.size();i++){
+            UserResponse response2 = new UserResponse(responsesFromFile.get(0).getUserId(), questionsFromFile.get(i),responsesFromFile.get(0).getResponseTexts().get(2));
+            respSet2.addResponse(response2);
+        }
+        assertEquals(33.33333333,respSet2.CalcScore(),OK_DOUBLE_MARGIN);
     }
 }
--- src/test/java/edu/ithaca/dragon/par/studentModel/UserResponseTest.java
+++ src/test/java/edu/ithaca/dragon/par/studentModel/UserResponseTest.java
@@ -1,6 +1,7 @@
 package edu.ithaca.dragon.par.studentModel;
 
 import edu.ithaca.dragon.par.domainModel.Question;
+import edu.ithaca.dragon.par.io.ImageTaskResponse;
 import edu.ithaca.dragon.util.JsonUtil;
 import org.junit.Test;
 
@@ -15,7 +16,8 @@ public class UserResponseTest {
     @Test
     public void checkResponseTest() throws IOException {
         List<Question> questionsFromFile = JsonUtil.listFromJsonFile("src/test/resources/author/SampleQuestions.json", Question.class);
-        UserResponse response=new UserResponse("user1",questionsFromFile.get(0),questionsFromFile.get(0).getCorrectAnswer());
+        List<ImageTaskResponse> responsesFromFile = JsonUtil.listFromJsonFile("src/test/resources/author/SampleResponses.json", ImageTaskResponse.class);
+        UserResponse response=new UserResponse("user1",questionsFromFile.get(0),responsesFromFile.get(0).getResponseTexts().get(0));
         assertEquals(response.checkResponse(),true);
 
     }
