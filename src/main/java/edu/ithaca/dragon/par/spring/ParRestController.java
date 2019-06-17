b6eecdd516117590122b1daa7836e601e6a6f75a
Aine <m.g.firefox@gmail.com>
2019-06-17 11:48:55 -0400

    read new image task

--- src/main/java/edu/ithaca/dragon/par/spring/ParRestController.java
+++ src/main/java/edu/ithaca/dragon/par/spring/ParRestController.java
@@ -2,9 +2,15 @@ package edu.ithaca.dragon.par.spring;
 
 import edu.ithaca.dragon.par.ParServer;
 import edu.ithaca.dragon.par.domainModel.Question;
+import edu.ithaca.dragon.util.JsonUtil;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -25,6 +31,21 @@ public class ParRestController {
     @GetMapping("/nextQuestion")
     public Question nextQuestion() {
         return parServer.nextQuestion();
+    }
+
+    @GetMapping("/nextImageTask")
+    public List<Question> nextImageTask() {
+        Logger logger = LogManager.getLogger(this.getClass());
+        try {
+            //change from sample questions to Image task once ImageTask has multiple questions
+            //what it is currently is just for testing
+            List<Question> questionsFromFile = JsonUtil.listFromJsonFile("src/test/resources/author/SampleQuestions.json", Question.class);
+            return questionsFromFile;
+        }
+        catch (IOException e){
+            logger.error("sample questions not found", e);
+            return null;
+        }
     }
 
 }
---
+++ src/main/resources/static/js/ReadImageTask.js
@@ -0,0 +1,92 @@
+var amountOfStructures=0;
+var amountOfAttachments=0;
+
+var QuestionTypes= [];
+var QuestionAnswers= [];
+
+
+function readJson(url){
+    var request = new XMLHttpRequest();
+    request.open("GET", url, false);
+    request.send(null);
+
+    return JSON.parse(request.response);
+}
+
+function readQuestion(question){
+
+    if(question.difficulty==1) {
+        document.getElementById('plane').innerHTML = setPlane(question.correctAnswer);
+        QuestionTypes.push("plane")
+        QuestionAnswers.push(question.correctAnswer)
+    }
+
+    if(question.difficulty==2) {
+        if (amountOfStructures != 0) {
+            document.getElementById('structure' + amountOfStructures).innerHTML = createFillIn(question, 'structure' + amountOfStructures);
+        } else {
+            document.getElementById('structure0').innerHTML = createFillIn(question, 'structure0');
+        }
+        QuestionTypes.push('structure' + amountOfStructures)
+        QuestionAnswers.push(question.correctAnswer)
+        amountOfStructures += 1;
+    }
+
+    if(question.difficulty==3) {
+        if (amountOfAttachments != 0) {
+            document.getElementById('attachment' + amountOfAttachments).innerHTML = createFillIn(question, 'attachment' + amountOfAttachments );
+        } else {
+            document.getElementById('attachment0').innerHTML = createFillIn(question,'attachment0' );
+        }
+        QuestionTypes.push('attachment' + amountOfStructures)
+        QuestionAnswers.push(question.correctAnswer)
+        amountOfAttachments += 1;
+    }
+
+    if(question.difficulty==4) {
+        document.getElementById('zone').innerHTML = createZoneQuestion(question);
+        QuestionTypes.push("zone")
+        QuestionAnswers.push(question.correctAnswer)
+    }
+}
+
+function setPlane(type){
+    if(type=="Transverse") {
+        return '<p>What plane is this?</p> <input type="radio" name="plane" value="Incorrect"> Lateral<br> <input type="radio" name="plane" value="Correct"> Transverse<br> <input type="radio" name="plane" value="Unsure"> I do not know<br>';
+    } else {
+        return '<p>What plane is this?</p> <input type="radio" name="plane" value="Correct"> Lateral<br> <input type="radio" name="plane" value="Incorrect"> Transverse<br> <input type="radio" name="plane" value="Unsure"> I do not know<br>';
+    }
+}
+
+function createZoneQuestion(json) {
+    var question= "<p> What zone is this? </p>";
+    for(var i = 0; i < json.answers.length; i++){
+        question+='<br> <input type="radio" name="zone" value=';
+        if(json.answers[i]==json.correctAnswer){
+            question+="Correct";
+            question+= json.answers[i];
+        } else {
+            question+="Inorrect";
+            question+= json.answers[i];
+        }
+    }
+    question+= '<br>'
+    return question;
+}
+
+function createFillIn(json, type) {
+    var question= "<p>" + json.questionText + '</p> <input name="'+ type + '" list="'+ type+ '"/> <datalist id="'+ type+'">';
+    for(var i = 0; i < json.answers.length; i++){
+        question= question +'<option value="' +json.answers[0] + '"/>';
+    }
+    question+= '</datalist>';
+    
+    return question;
+}
+
+function changeQuestions() {
+    clearpage();
+
+    var question = readJson("api/nextImageTask")
+    for (var i = 0; i < question.length; i++) readQuestion(question.get(i));
+}
