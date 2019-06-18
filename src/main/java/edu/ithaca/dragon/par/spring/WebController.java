ceb47367a2964eaae9fc5d49ea3297e0cda06ae4
Aine <m.g.firefox@gmail.com>
2019-06-18 15:10:12 -0400

    mode select page

--- src/main/java/edu/ithaca/dragon/par/spring/WebController.java
+++ src/main/java/edu/ithaca/dragon/par/spring/WebController.java
@@ -2,14 +2,24 @@ package edu.ithaca.dragon.par.spring;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Controller
 public class WebController implements WebMvcConfigurer {
 
-    @RequestMapping("/start")
-    public String greetingForm() {
+    @RequestMapping("/chooseMode")
+    public String chooseMode() {
+        return "ModeSelectPage";
+    }
+
+    @RequestMapping(value = "/redirect", method = RequestMethod.GET)
+    public String redirect() {
+        return "redirect:finalPage";
+    }
+    @RequestMapping(value = "/finalPage", method = RequestMethod.GET)
+    public String finalPage() {
         return "ImageTaskTemplate";
     }
 }
