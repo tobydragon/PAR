c5ac7e2a360d23171a4954055c1e04e7ca79a2c0
Aine <m.g.firefox@gmail.com>
2019-06-18 12:01:41 -0400

    response sends ImageTaskResponse back to server

--- src/main/java/edu/ithaca/dragon/par/ParServer.java
+++ src/main/java/edu/ithaca/dragon/par/ParServer.java
@@ -1,24 +1,12 @@
 package edu.ithaca.dragon.par;
 
-import edu.ithaca.dragon.par.domainModel.Question;
-import edu.ithaca.dragon.util.JsonUtil;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import edu.ithaca.dragon.par.io.ImageTaskResponse;
 
 import java.io.IOException;
-import java.util.List;
 
 public class ParServer {
-    private  Logger logger = LogManager.getLogger(this.getClass());
-
-    public Question nextQuestion() {
-        try {
-            List<Question> questionsFromFile = JsonUtil.listFromJsonFile("src/test/resources/author/SampleQuestions.json", Question.class);
-            return questionsFromFile.get(0);
-        }
-        catch (IOException e){
-            logger.error("sample questions not found", e);
-            return null;
-        }
+    public static String sendNewImageTaskResponse(ImageTaskResponse response) throws IOException{
+        //update the data store with the new imageTaskResponse
+        return null;
     }
 }
