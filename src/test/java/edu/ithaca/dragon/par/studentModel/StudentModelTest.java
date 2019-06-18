0d76efc9a90b11320e83ed3da3a33e3184ade323
KandacePC <krichards1@ithaca.edu>
2019-06-18 14:35:54 -0400

    format change-minor

--- src/test/java/edu/ithaca/dragon/par/studentModel/StudentModelTest.java
+++ src/test/java/edu/ithaca/dragon/par/studentModel/StudentModelTest.java
@@ -17,13 +17,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StudentModelTest {
 
     StudentModel studentModel;
-    @Test
-    public void imageTaskResponseSubmittedTest() throws IOException{
-        QuestionPool qp = new QuestionPool(new JsonDatastore("src/test/resources/author/SampleQuestions.json"));
-        List<ImageTaskResponse> responsesFromFile = JsonUtil.listFromJsonFile("src/test/resources/author/SampleResponses.json", ImageTaskResponse.class);
-        studentModel.imageTaskResponseSubmitted(responsesFromFile.get(0),qp);
-        assertEquals(studentModel.getResponseCount(), 3);
-    }
+
     @BeforeEach
     public void setUp() throws IOException{
         List<Question> questionsFromFile = JsonUtil.listFromJsonFile("src/test/resources/author/SampleQuestions.json", Question.class);
@@ -41,5 +35,12 @@ public class StudentModelTest {
         assertEquals(3, studentModel.getUnseenQuestionCount());
         assertEquals(0, studentModel.getSeenQuestionCount());
         assertEquals(0, studentModel.getResponseCount());
+    }
+    @Test
+    public void imageTaskResponseSubmittedTest() throws IOException{
+        QuestionPool qp = new QuestionPool(new JsonDatastore("src/test/resources/author/SampleQuestions.json"));
+        List<ImageTaskResponse> responsesFromFile = JsonUtil.listFromJsonFile("src/test/resources/author/SampleResponses.json", ImageTaskResponse.class);
+        studentModel.imageTaskResponseSubmitted(responsesFromFile.get(0),qp);
+        assertEquals(studentModel.getResponseCount(), 3);
     }
 }
