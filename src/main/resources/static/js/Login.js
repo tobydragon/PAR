78cf1c25ae924e4c9a1be900d18422534233bf86
Emma <emurtagh03@gmail.com>
2019-07-01 09:52:24 -0400

    added login via enter button

--- src/main/resources/static/js/Login.js
+++ src/main/resources/static/js/Login.js
@@ -10,3 +10,14 @@ function change() {
     setUserId();
     return location.replace('/imageTaskView?userId=' + User);
 }
+
+function enterSubmit() {
+    console.log("enterSubmit");
+    document.getElementById('inputId').onkeydown = function (e) {
+        if (e.keyCode == 13) {
+            e.preventDefault();
+            change();
+        }
+
+    }
+}
