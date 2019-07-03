04e3fc2a7a87cfeac7fa54114ba90e5efb2f3eb0
kerrybuckman <kbuckman@ithaca.edu>
2019-07-03 09:42:51 -0400

    DemoQuestionPool.json added, getInitialQuestionForTask changed to take level into account

--- src/test/java/edu/ithaca/dragon/par/pedagogicalModel/TaskGeneratorTest.java
+++ src/test/java/edu/ithaca/dragon/par/pedagogicalModel/TaskGeneratorTest.java
@@ -29,36 +29,36 @@ public class TaskGeneratorTest {
         assertEquals(15, studentModel.getUnseenQuestionCount());
 
         //make an imageTask and check aspects of it
-        Question task1Question = TaskGenerator.getInitialQuestionForTask(studentModel);
+        Question task1Question = TaskGenerator.getInitialQuestionForTask(studentModel, 1);
         ImageTask task1 = new ImageTask(task1Question.getImageUrl(), Arrays.asList(task1Question));
         assertEquals("./images/demoEquine04.jpg", task1.getImageUrl());
         assertEquals(1, task1.getTaskQuestions().size());
 
         //make a new imageTask and check aspects of it
-        Question task2Question = TaskGenerator.getInitialQuestionForTask(studentModel);
+        Question task2Question = TaskGenerator.getInitialQuestionForTask(studentModel, 1);
         ImageTask task2 = new ImageTask(task2Question.getImageUrl(), Arrays.asList(task1Question));
         assertEquals("./images/demoEquine04.jpg", task1.getImageUrl());
         assertEquals(1, task2.getTaskQuestions().size());
-        System.out.println(task2.getImageUrl());
+
     }
 
     @Test
     public void makeTaskTest() throws IOException{
         //set up questionPool and studentModel, create an imageTask with the studentModel
-        QuestionPool questionPool = new QuestionPool(new JsonDatastore("src/test/resources/author/SampleQuestionPool.json"));
+        QuestionPool questionPool = new QuestionPool(new JsonDatastore("src/test/resources/author/DemoQuestionPool.json"));
         StudentModel studentModel = new StudentModel("TestUser1", questionPool.getAllQuestions());
 
         //make an imageTask and check aspects of it
         ImageTask task1 = TaskGenerator.makeTask(studentModel);
 
-        assertEquals("./images/demoEquine04.jpg", task1.getImageUrl());
+        assertEquals("./images/demoEquine14.jpg", task1.getImageUrl());
 
-        assertEquals(2, task1.getTaskQuestions().size());
+        assertEquals(4, task1.getTaskQuestions().size());
 
         //make a new imageTask and check aspects of it
         ImageTask task2 = TaskGenerator.makeTask(studentModel);
         assertEquals("./images/demoEquine02.jpg", task2.getImageUrl());
-        assertEquals(1, task2.getTaskQuestions().size());
+        assertEquals(5, task2.getTaskQuestions().size());
 
     }
 
@@ -69,7 +69,7 @@ public class TaskGeneratorTest {
 
         //try to make a single Question
         try{
-            Question newQ = TaskGenerator.getInitialQuestionForTask(studentModel);
+            Question newQ = TaskGenerator.getInitialQuestionForTask(studentModel, 1);
             fail();
         }catch(Exception ee){
 
@@ -108,6 +108,77 @@ public class TaskGeneratorTest {
         questions = TaskGenerator.removeType(questions, "Zone");
         assertEquals(0, questions.size());
 
+
+    }
+
+    @Test
+    public void addAllQuestionsTest()throws IOException{
+        QuestionPool questionPool = new QuestionPool(new JsonDatastore("src/test/resources/author/DemoQuestionPool.json"));
+        StudentModel studentModel = new StudentModel("TestUser1", questionPool.getAllQuestions());
+
+        //first url
+        Question q1 = studentModel.getUserQuestionSet().getUnseenQuestions().get(0);
+        List<Question> qs =TaskGenerator.addAllQuestions(studentModel, q1);
+        assertEquals(5, qs.size());
+
+        //second url
+        Question q2 = studentModel.getUserQuestionSet().getUnseenQuestions().get(5);
+        qs =TaskGenerator.addAllQuestions(studentModel, q2);
+        assertEquals(6, qs.size());
+
+        //third url
+        Question q3 = studentModel.getUserQuestionSet().getUnseenQuestions().get(11);
+        qs =TaskGenerator.addAllQuestions(studentModel, q3);
+        assertEquals(4, qs.size());
+
+        //fourth url
+        Question q4 = studentModel.getUserQuestionSet().getUnseenQuestions().get(15);
+        qs =TaskGenerator.addAllQuestions(studentModel, q4);
+        assertEquals(3, qs.size());
+        //repeat
+        qs =TaskGenerator.addAllQuestions(studentModel, q4);
+        assertEquals(3, qs.size());
+        qs =TaskGenerator.addAllQuestions(studentModel, q4);
+        assertEquals(3, qs.size());
+
+        //last question
+        Question q5 = studentModel.getUserQuestionSet().getUnseenQuestions().get(questionPool.getAllQuestions().size()-1);
+        qs =TaskGenerator.addAllQuestions(studentModel, q5);
+        assertEquals(6, qs.size());
+    }
+
+    @Test
+    public void filterQuestionsTest()throws IOException{
+       //plane only
+        QuestionPool questionPool = new QuestionPool(new JsonDatastore("src/test/resources/author/DemoQuestionPool.json"));
+        List<Question> questionList = questionPool.getAllQuestions();
+        questionList = TaskGenerator.filterQuestions(1, questionList);
+        assertEquals(10, questionList.size());
+
+        //plane and structure only
+        questionList = questionPool.getAllQuestions();
+        questionList = TaskGenerator.filterQuestions(2, questionList);
+        assertEquals(37, questionList.size());
+
+        //structure only
+        questionList = questionPool.getAllQuestions();
+        questionList = TaskGenerator.filterQuestions(3, questionList);
+        assertEquals(27, questionList.size());
+
+        //structure and attachment only
+        questionList = questionPool.getAllQuestions();
+        questionList = TaskGenerator.filterQuestions(4, questionList);
+        assertEquals(27, questionList.size());
+
+        //structure, attachment, and zone only
+        questionList = questionPool.getAllQuestions();
+        questionList = TaskGenerator.filterQuestions(5, questionList);
+        assertEquals(37, questionList.size());
+
+        //zone only
+        questionList = questionPool.getAllQuestions();
+        questionList = TaskGenerator.filterQuestions(6, questionList);
+        assertEquals(10, questionList.size());
 
     }
 }
