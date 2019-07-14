d5a338f7f15241e48257a06ef67c9801b218c59f
Toby Dragon <dr.toby.dragon@gmail.com>
2019-07-14 14:36:22 -0400

    one test working

--- src/test/javascript/spec/TextEntryResponseBoxTest.js
+++ src/test/javascript/spec/TextEntryResponseBoxTest.js
@@ -3,6 +3,6 @@
 describe("TextEntryResponseBox", function () {
     it("buildOptionElement", function () {
         let optionElement = buildOptionElement("nope");
-        expect(optionElement.getAttribute("value").toBe("nope"));
+        expect(optionElement.getAttribute("value")).toBe("nope");
     });
 });
