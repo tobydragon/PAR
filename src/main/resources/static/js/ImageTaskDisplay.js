3ac2f873a25d7ad847472d583d395f7ad8267403
Aine <m.g.firefox@gmail.com>
2019-07-16 11:02:55 -0400

    changed nextQuestion button to be a new function

--- src/main/resources/static/js/ImageTaskDisplay.js
+++ src/main/resources/static/js/ImageTaskDisplay.js
@@ -17,8 +17,11 @@ class ImageTaskDisplay{
             this.questionAreaDisp[i].answerBox.checkCurrentResponse(this.response);
         }
 
-        console.log(this.response.taskQuestionIds);
         submitToAPI("api/recordResponse", this.response);
+    }
+
+    nextQuestion(){
+        location.reload();
     }
 
 }
