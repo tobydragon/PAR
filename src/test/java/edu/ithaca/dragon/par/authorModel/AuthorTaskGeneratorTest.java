cd3999ed8d16f16f36b4b5f51ddb0433de60bdf5
kerrybuckman <kbuckman@ithaca.edu>
2019-07-16 11:17:18 -0400

    AuthorTaskGenerator and AuthorTaskGeneratorTest created, empty functions added.

---
+++ src/test/java/edu/ithaca/dragon/par/authorModel/AuthorTaskGeneratorTest.java
@@ -0,0 +1,21 @@
+package edu.ithaca.dragon.par.authorModel;
+
+import org.junit.jupiter.api.Test;
+
+public class AuthorTaskGeneratorTest {
+
+    @Test
+    public void nextImageTaskTemplateTest(){
+
+    }
+
+    @Test
+    void getInitialQuestionTest() {
+
+    }
+
+    @Test
+    void getAllQuestionsWithSameUrlTest() {
+
+    }
+}
