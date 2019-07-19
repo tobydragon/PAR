91f443cf9c8deb75b2474bd43171f40699dadb20
Emma <emurtagh03@gmail.com>
2019-07-19 10:47:50 -0400

    updated image task template, with more horses

--- src/main/resources/static/js/Login.js
+++ src/main/resources/static/js/Login.js
@@ -1,5 +1,5 @@
 var User;
-var bottomText = ["Ready... Set... PAR!", "You're going to be amazing!", "A PAR employee will never ask for your password", "Are you a dedicated horse person?", "Are you ready to rumble?????"];
+var bottomText = ["Ready... Set... PAR!", "You're going to be amazing!", "A PAR employee will never ask for your password", "Are you a dedicated horse person?", "Are you ready to rumble?????", "Lights, Camera, HORSES!", "Yaay!!!", "Now with 100% more Horses!", "920814850!", "Horses go NEIGH!", "Baked with love and bytes!", "Dogmouth!"];
 
 
 function getRandomNumber(num) {
@@ -33,7 +33,7 @@ function enterSubmit() {
     document.getElementById('inputId').onkeydown = function (e) {
         //13 is enter key code
         if (e.keyCode == 13) {
-            //preventDevault() cancells current action, in this case the page refreshing and allows the login function to be called
+            //preventDevault() cancels current action, in this case the page refreshing and allows the login function to be called
             e.preventDefault();
             login();
         }
