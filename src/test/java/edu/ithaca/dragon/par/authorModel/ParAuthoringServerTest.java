4fb6e20bdca5a017a0b635680e61cc0d26a0383d
John Hunter <jhunter1@ithaca.edu>
2019-07-22 14:50:37 -0400

    Added endOfTasksTest to test the process of going through questions as an author an eventually running out

--- src/test/java/edu/ithaca/dragon/par/authorModel/ParAuthoringServerTest.java
+++ src/test/java/edu/ithaca/dragon/par/authorModel/ParAuthoringServerTest.java
@@ -145,48 +145,33 @@ public class ParAuthoringServerTest {
 
     @Test
     public void endOfTasksTest() throws IOException{
+        datastore = new JsonDatastore("src/test/resources/author/SampleQuestionsEmpty.json");
+        templateDatastore = new JsonDatastore("src/test/resources/author/DemoQuestionPoolTemplateSmall.json");
+        pas = new ParAuthoringServer(datastore, templateDatastore);
+
         ImageTask imageTask = pas.nextImageTaskTemplate();
-        pas.imageTaskResponseSubmitted(new ImageTaskResponse());
-
-
-        assertEquals(5, imageTask.getTaskQuestions().size());
         assertEquals("./images/demoEquine14.jpg", imageTask.getImageUrl());
+        assertEquals(5, imageTask.getTaskQuestions().size());
+        pas.imageTaskResponseSubmitted(new ImageTaskResponse("Author1", Arrays.asList("plane./images/demoEquine14.jpg", "structure0./images/demoEquine14.jpg", "AttachQ1", "AttachQ2"), Arrays.asList("longitudinal", "Superficial digital flexor endon", "Type1", "Type2")));
 
         ImageTask imageTask2 = pas.nextImageTaskTemplate();
-        assertEquals(6, imageTask2.getTaskQuestions().size());
-        assertEquals("./images/demoEquine02.jpg", imageTask2.getImageUrl());
+        assertEquals("./images/demoEquine04.jpg", imageTask2.getImageUrl());
+        assertEquals(3, imageTask2.getTaskQuestions().size());
+        pas.imageTaskResponseSubmitted(new ImageTaskResponse("Author1", Arrays.asList("plane./images/demoEquine04.jpg", "structure0./images/demoEquine04.jpg", "AttachQ5", "zone./images/demoEquine04.jpg"), Arrays.asList("longitudinal", "Superficial digital flexor endon", "Type1", "3c")));
 
         ImageTask imageTask3 = pas.nextImageTaskTemplate();
-        assertEquals(4, imageTask3.getTaskQuestions().size());
-        assertEquals("./images/demoEquine13.jpg", imageTask3.getImageUrl());
+        assertEquals("./images/demoEquine14.jpg", imageTask3.getImageUrl());
+        assertEquals(3, imageTask3.getTaskQuestions().size());
 
         ImageTask imageTask4 = pas.nextImageTaskTemplate();
-        assertEquals(3, imageTask4.getTaskQuestions().size());
-        assertEquals("./images/demoEquine04.jpg", imageTask4.getImageUrl());
+        assertEquals("./images/demoEquine14.jpg", imageTask4.getImageUrl());
+        assertEquals(3, imageTask3.getTaskQuestions().size());
+        pas.imageTaskResponseSubmitted(new ImageTaskResponse("Author1", Arrays.asList("structure2./images/demoEquine14.jpg","structure3./images/demoEquine14.jpg", "zone./images/demoEquine14.jpg"), Arrays.asList("Suspensory ligament (branches)", "Proximal sesamoid bones", "1A")));
 
+        //no more template questions
         ImageTask imageTask5 = pas.nextImageTaskTemplate();
-        assertEquals(6, imageTask5.getTaskQuestions().size());
-        assertEquals("./images/demoEquine10.jpg", imageTask5.getImageUrl());
-
-        ImageTask imageTask6 = pas.nextImageTaskTemplate();
-        assertEquals(4, imageTask6.getTaskQuestions().size());
-        assertEquals("./images/demoEquine11.jpg", imageTask6.getImageUrl());
-
-        ImageTask imageTask7 = pas.nextImageTaskTemplate();
-        assertEquals(5, imageTask7.getTaskQuestions().size());
-        assertEquals("./images/demoEquine05.jpg", imageTask7.getImageUrl());
-
-        ImageTask imageTask8 = pas.nextImageTaskTemplate();
-        assertEquals(5, imageTask8.getTaskQuestions().size());
-        assertEquals("./images/demoEquine09.jpg", imageTask8.getImageUrl());
-
-        ImageTask imageTask9 = pas.nextImageTaskTemplate();
-        assertEquals(3, imageTask9.getTaskQuestions().size());
-        assertEquals("./images/demoEquine37.jpg", imageTask9.getImageUrl());
-
-        ImageTask imageTask10 = pas.nextImageTaskTemplate();
-        assertEquals(6, imageTask10.getTaskQuestions().size());
-        assertEquals("./images/demoEquine32.jpg", imageTask10.getImageUrl());
+        assertEquals("noMoreQuestions", imageTask5.getImageUrl());
+        assertEquals(0, imageTask5.getTaskQuestions().size());
     }
 
 }
