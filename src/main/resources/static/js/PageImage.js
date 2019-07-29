d9e105f9330efae042fd21a1e3c9438345a9a026
Emma <emurtagh03@gmail.com>
2019-07-29 13:31:23 -0400

    trying to get some of this canvas stuff to work

--- src/main/resources/static/js/PageImage.js
+++ src/main/resources/static/js/PageImage.js
@@ -1,51 +1,76 @@
-class PageImage{
+class PageImage {
 
     constructor(imageUrl, canvasName) {
         //Check to see if the canvas has a context
         if (!canvasSupport()) {
             return; //Canvas not supported so exit the function
         }
-
-        var image;
-        var imageSource;
-
-        function loadImages(images, imageSources, callback) {
-            imageSource = imageUrl;
-
-            image = new Image();
-            image.onload = function () {
-                callback()
-            };
-            image.src = imageSource;
-
-        }
-
-        //Setup the canvas object
-        var theCanvas = document.getElementById(canvasName); //get the canvas element
-        var context = theCanvas.getContext("2d"); //get the context
-        var canvasWidth = theCanvas.width; //get the width of the canvas
-        var canvasHeight = theCanvas.height; //get the heigth of the canvas
-        var canvasColor = "white"; // set the default canvas bg color
-
-        function clearCanvas(canvasColor) {
-            context.fillStyle = canvasColor;
-            context.fillRect(0, 0, canvasWidth, canvasHeight);
-        }
-
-        function drawCanvas() {
-            clearCanvas(canvasColor);
-            context.drawImage(image, 0, 0, canvasWidth, canvasHeight);
-        }
-
-        loadImages(image, imageSource, function () {
-            drawCanvas()
-        });
-
+        this.canvas = createCanvas(this.imageUrl, this.canvasName);
+        this.loadImages = loadImageOnCanvas(this.imageUrl, this.canvas);
+        console.log(displayElement(createDisplayElement(this.canvas)));
+        this.element = displayElement(createDisplayElement(this.canvas));
     }
+}
+
+function loadImageOnCanvas(imageUrl, canvas) {
+    let image;
+    let imageSource;
+
+    function loadImages(image, imageSource, callback) {
+        imageSource = imageUrl;
+
+        image = new Image();
+        image.onload = function () {
+            callback()
+        };
+        image.src = imageSource;
+    }
+
+    //Setup the canvas object
+    let context = canvas.getContext("2d"); //get the context
+    let canvasWidth = canvas.width; //get the width of the canvas
+    let canvasHeight = canvas.height; //get the heigth of the canvas
+    let canvasColor = "white"; // set the default canvas bg color
+
+    function clearCanvas(canvasColor) {
+        context.fillStyle = canvasColor;
+        context.fillRect(0, 0, canvasWidth, canvasHeight);
+    }
+
+    function drawCanvas() {
+        clearCanvas(canvasColor);
+        context.drawImage(image, 0, 0, canvasWidth, canvasHeight);
+    }
+
+    loadImages(image, imageSource, function () {
+        drawCanvas();
+    });
+}
+
+function createCanvas(imageUrl, name) {
+    let newCanvas = document.createElement("CANVAS");
+    newCanvas.id = name;
+    newCanvas.width = "1024";
+    newCanvas.height = "768";
+    //newCanvas.classList.add("center-block");
+    newCanvas.classList.add("canvas");
+    return newCanvas;
+}
+
+function createDisplayElement(canvas) {
+    let canvasDisplayElement = document.createElement('div');
+    canvasDisplayElement.classList.add('col-12');
+    canvasDisplayElement.appendChild(canvas);
+    return canvasDisplayElement;
+}
+
+function displayElement(elementToDisplay) {
+    let outerNode = document.createElement('div');
+    outerNode.classList.add('row');
+    outerNode.appendChild(elementToDisplay);
+    return outerNode;
 }
 
 function canvasSupport() {
     return Modernizr.canvas;
 }
-
-
