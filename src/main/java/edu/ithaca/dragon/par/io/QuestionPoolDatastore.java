d67613a503435a7fbf216c640904cda6aae5afe7
John Hunter <jhunter1@ithaca.edu>
2019-08-12 13:23:56 -0400

    Created the QuestionPoolDatastore interface

---
+++ src/main/java/edu/ithaca/dragon/par/io/QuestionPoolDatastore.java
@@ -0,0 +1,14 @@
+package edu.ithaca.dragon.par.io;
+
+import edu.ithaca.dragon.par.domainModel.Question;
+
+import java.io.IOException;
+import java.util.List;
+
+public interface QuestionPoolDatastore {
+
+    void addQuestion(Question newQuestion) throws IOException;
+
+    void removeQuestionById(String questionId) throws IOException;
+    List<Question> removeAllQuestions() throws IOException;
+}
