891bd4642d3c124fa7e89a7bd7e292afc9a7314c
Emma <emurtagh03@gmail.com>
2019-10-14 11:48:26 -0400

    Revert "started migration from comp disp to page display. cleared out old modal code in login page html"
    This reverts commit ff49a37f908a1f102b456f7ec8dc170d11bed91f.

--- src/test/javascript/spec/PageDisplayTest.js
+++ src/test/javascript/spec/PageDisplayTest.js
@@ -24,31 +24,8 @@ describe("PageDisplay", function () {
 
         expect(imageTaskElement.getAttribute('class')).toContain('container-fluid');
         expect(imageTaskElement.childNodes.item(1).getAttribute('class')).toContain('row');
-    });
-    it("createPageDisplay", function(){
-        let listOfImageTasks = readJson("../resources/author/SampleListOfImageTasks.json");
-        let imageTaskSettings = {
-            unsureShowsCorrectAnswer: "False",
-            feedbackByType: {
-                "plane": "Look at chapter 12",
-                structure: "View notes from 9/25",
-                attachment: "Read Page 394",
-                zone: "Rewatch <a href=https://www.youtube.com/watch?v=dQw4w9WgXcQ> this video </a>"
-            },
-            ableToResubmitAnswers: "True",
-            mustSubmitAnswersToContinue: "False",
-            canGiveNoAnswer: "True",
-            willDisplayFeedback: "False"
-        };
-        let pageDisplaySettings = {
-            "showScore": false,
-            "scoreType": 'NumberByType'
-        };
-        let imageTaskDisplayObject = new ImageTaskDisplay(listOfImageTasks[0], "testUser", imageTaskSettings, true, "canvas0", pageDisplaySettings);
-        let pageDisplayObject=new PageDisplay(pageDisplaySettings);
-        let createPageDisplay=pageDisplayObject.createPageDisplay();
-        expect(createPageDisplay).not.toBe(null);
+
+
 
     });
-
 });
