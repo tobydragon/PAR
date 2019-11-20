d59c86e3daf3591bada297f00b93f26abce1ad1f
Emma <emurtagh03@gmail.com>
2019-11-20 17:09:51 -0500

    added final test, need to finish it

--- src/test/javascript/spec/QuestionAndAnswerViewTest.js
+++ src/test/javascript/spec/QuestionAndAnswerViewTest.js
@@ -15,5 +15,16 @@ describe("QuestionAndAnswerView", function () {
         qaavTestObj.questionView.element.value="What is this structure?";
         expect(qaavTestObj.getResponse()).not.toBeNull();
     });
-
+    //No test needed for checkAndUpdateView as test functionality already shown in the AnswerViewTest.
+    it("buildQuestionAndAnswerViewElement", function(){
+        let qaModel = {
+            id: "qa31",
+            questionText:null,
+            possibleAnswers: ["transverse", "Both proximal and middle phalanxes"],
+            correctAnswer: "transverse"
+        };
+        let qaavTestObj = new QuestionAndAnswerView(qaModel);
+        let buildQandAViewTestElement=buildQuestionAndAnswerViewElement(qaModel.id,qaavTestObj.questionView.element,qaavTestObj.answerView.inputTextbox);
+        expect(buildQandAViewTestElement).not.toBeNull();
+    });
 });
