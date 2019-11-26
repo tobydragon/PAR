5a115647f7d9b67fe1451e8bb143344c6f8e8ecf
Emma <emurtagh03@gmail.com>
2019-11-26 11:45:46 -0500

    fixed QuestionAndAnswerController tests

--- src/test/javascript/spec/QuestionAndAnswerControllerTest.js
+++ src/test/javascript/spec/QuestionAndAnswerControllerTest.js
@@ -2,18 +2,26 @@
 
 describe("QuestionAndAnswerController", function () {
     it("getResponse", function(){
-        let qaModel = {
+        //first check to be null
+        let qaModel0 = {
             id: "qa31",
             questionText:null,
             possibleAnswers: ["transverse", "Both proximal and middle phalanxes"],
             correctAnswer: "transverse"
-
         };
-        let qaavTestObj = new QuestionAndAnswerController(qaModel);
+        let qaavTestObj = new QuestionAndAnswerController(qaModel0);
         expect(qaavTestObj.getResponse()).toBeNull();
-        qaavTestObj.answerView.inputTextbox.value="transverse";
-        qaavTestObj.questionView.element.value="What is this structure?";
-        expect(qaavTestObj.getResponse()).not.toBeNull();
+
+        //second check to not be null
+        let qaModel1 = {
+            id: "qa31",
+            questionText:"What is this structure",
+            possibleAnswers: ["transverse", "Both proximal and middle phalanxes"],
+            correctAnswer: "transverse"
+        };
+        let qaavTestObj2=new QuestionAndAnswerController(qaModel1);
+        qaavTestObj2.answerView.inputTextbox.value="transverse";
+        expect(qaavTestObj2.getResponse()).not.toBeNull();
     });
     //No test needed for checkAndUpdateView as test functionality already shown in the AnswerViewTest.
     it("buildQuestionAndAnswerViewElement", function(){
