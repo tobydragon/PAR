d016f14efb4afa34171379d47c848b864220193e
kerrybuckman <kbuckman@ithaca.edu>
2020-05-06 05:32:43 -0400

    created MessageGenerator class and test class

---
+++ src/main/java/edu/ithaca/dragon/par/pedagogicalModel/MessageGenerator.java
@@ -0,0 +1,11 @@
+package edu.ithaca.dragon.par.pedagogicalModel;
+
+import edu.ithaca.dragon.par.io.ImageTask;
+
+public class MessageGenerator {
+    public static ImageTask generateMessage(ImageTask imageTask){
+
+
+        return imageTask;
+    }
+}
---
+++ src/test/java/edu/ithaca/dragon/par/pedagogicalModel/MessageGeneratorTest.java
@@ -0,0 +1,10 @@
+package edu.ithaca.dragon.par.pedagogicalModel;
+
+import org.junit.jupiter.api.Test;
+
+public class MessageGeneratorTest {
+    @Test
+    public void generateMessageTest(){
+
+    }
+}
