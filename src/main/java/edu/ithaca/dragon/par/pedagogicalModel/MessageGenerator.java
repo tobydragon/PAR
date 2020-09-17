f48aeff493d4296a435163795987b1dadeb2d7e6
kerrybuckman <kbuckman@ithaca.edu>
2020-09-17 13:29:35 -0400

    fixed repeating question method

--- src/main/java/edu/ithaca/dragon/par/pedagogicalModel/MessageGenerator.java
+++ src/main/java/edu/ithaca/dragon/par/pedagogicalModel/MessageGenerator.java
@@ -15,7 +15,10 @@ public class MessageGenerator {
     private static final String masterNoRepeat = "You have mastered the material, feel free to keep practicing";
     //note: decreaseLevel and repeatLevel messages can't be a private static final Strings because it has more personalized feedback
 
-    private static final int repeatWindow = 180000; //30 min in mili
+    //TODO: can make the pieces of the strings private static final
+
+
+    private static final int repeatWindow = 1800000; //30 min in mili
 
     public static void generateMessage(StudentModel studentModel, ImageTask imageTask, int previousLevel){
         int level = studentModel.getLastLevelRecorded();
@@ -112,7 +115,7 @@ public class MessageGenerator {
             int ind = -1;
             List<Question> allQuestions = studentModel.getUserQuestionSet().getAllQuestions();
             for (int i = 0; i < allQuestions.size(); i++){
-                //if the id of the current question equals that of the first in the task
+                //if the id of the current question equals the first in the task
                 if (allQuestions.get(i).getId().equals(imageTask.getTaskQuestions().get(0).getId())){
                     ind = i;
                 }
@@ -124,16 +127,14 @@ public class MessageGenerator {
                     imageTask.setMessage(null);
                 }
                 else {
-                    //current milliseconds
-
-                    //if there's multiple responses
-                    if (responsesPerQuestion.getAllResponses().size() >= 2) {
-                        //second most recent response milliseconds
-                        long sec2 = responsesPerQuestion.getAllResponses().get(responsesPerQuestion.getAllResponses().size() - 2).getMillSeconds();
+                    //if there's at least one response
+                    if (responsesPerQuestion.getAllResponses().size() >= 1) {
+                        //most recent response milliseconds
+                        long sec2 = responsesPerQuestion.getAllResponses().get(responsesPerQuestion.getAllResponses().size() - 1).getMillSeconds();
 
                         //seen within the time window
                         Date date = new Date();
-                        if (date.getTime() - sec2 >= repeatWindow) {
+                        if (date.getTime() - sec2 <= repeatWindow) {
                             List<String> questionsInLevel = EquineQuestionTypes.getTypesForLevel(level);
                             String questionsOneString = "";
                             for (String q : questionsInLevel) {
--- src/test/java/edu/ithaca/dragon/par/pedagogicalModel/MessageGeneratorTest.java
+++ src/test/java/edu/ithaca/dragon/par/pedagogicalModel/MessageGeneratorTest.java
@@ -6,12 +6,16 @@ import edu.ithaca.dragon.par.domainModel.equineUltrasound.EquineQuestionTypes;
 import edu.ithaca.dragon.par.io.ImageTask;
 import edu.ithaca.dragon.par.io.JsonQuestionPoolDatastore;
 import edu.ithaca.dragon.par.io.StudentModelRecord;
+import edu.ithaca.dragon.par.studentModel.QuestionResponse;
+import edu.ithaca.dragon.par.studentModel.ResponsesPerQuestion;
 import edu.ithaca.dragon.par.studentModel.StudentModel;
 import edu.ithaca.dragon.util.JsonUtil;
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
 
 
 public class MessageGeneratorTest {
@@ -228,26 +232,50 @@ public class MessageGeneratorTest {
         //repeated within 30 min, message
         StudentModelRecord  smr30 = JsonUtil.fromJsonFile("src/test/resources/author/students/RepeatAfter30MinTest.json", StudentModelRecord.class);
         StudentModel student30 = smr30.buildStudentModel(myQP);
+
+        Date date = new Date();
+        for (ResponsesPerQuestion response:student30.getUserResponseSet().getResponsesPerQuestionList()){
+            List<QuestionResponse> r = response.getAllResponses();
+            QuestionResponse last = r.get(response.getAllResponses().size()-1);
+            last.setMillSeconds(date.getTime()-1799500);
+            response.setAllResponses(r);
+        }
+
         ImageTask it30 = taskGenerator.makeTask(student30, 4);
         student30.setLastLevelRecorded(LevelTaskGenerator.calcLevel(student30.calcKnowledgeEstimateByType(4)));
         MessageGenerator.repeatLevelMessage(student30, it30, 2);
         assertEquals("You've seen this question recently, you might be stuck on plane/structure questions.", it30.getMessage());
 
-        //repeated within 29 min, no message
+        //repeated within 29 min, message
         StudentModelRecord  smr29 = JsonUtil.fromJsonFile("src/test/resources/author/students/RepeatAfter30MinTest.json", StudentModelRecord.class);
         StudentModel student29 = smr29.buildStudentModel(myQP);
+
+        for (ResponsesPerQuestion response:student29.getUserResponseSet().getResponsesPerQuestionList()){
+            List<QuestionResponse> r = response.getAllResponses();
+            QuestionResponse last = r.get(response.getAllResponses().size()-1);
+            last.setMillSeconds(date.getTime()-1740000);
+            response.setAllResponses(r);
+        }
         ImageTask it29 = taskGenerator.makeTask(student29, 4);
         student29.setLastLevelRecorded(LevelTaskGenerator.calcLevel(student29.calcKnowledgeEstimateByType(4)));
         MessageGenerator.repeatLevelMessage(student29, it29, 2);
-        assertNull(it29.getMessage());
+        assertEquals("You've seen this question recently, you might be stuck on plane/structure questions.", it29.getMessage());
 
-        //repeated within 31 min, message
+        //repeated within 31 min, no message
         StudentModelRecord  smr31 = JsonUtil.fromJsonFile("src/test/resources/author/students/RepeatAfter30MinTest.json", StudentModelRecord.class);
         StudentModel student31 = smr31.buildStudentModel(myQP);
+
+        for (ResponsesPerQuestion response:student31.getUserResponseSet().getResponsesPerQuestionList()){
+            List<QuestionResponse> r = response.getAllResponses();
+            QuestionResponse last = r.get(response.getAllResponses().size()-1);
+            last.setMillSeconds(date.getTime()-1860000);
+            response.setAllResponses(r);
+        }
+
         ImageTask it31 = taskGenerator.makeTask(student31, 4);
         student31.setLastLevelRecorded(LevelTaskGenerator.calcLevel(student31.calcKnowledgeEstimateByType(4)));
         MessageGenerator.repeatLevelMessage(student31, it31, 2);
-        assertEquals("You've seen this question recently, you might be stuck on plane/structure questions.", it31.getMessage());
+        assertNull(it31.getMessage());
 
         //not repeated, no message
         StudentModelRecord  smr = JsonUtil.fromJsonFile("src/test/resources/author/students/buckmank.json", StudentModelRecord.class);
