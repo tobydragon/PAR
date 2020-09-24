d186a31cf1231145517c855ba16de7b7ece81f41
TimMarotta <timothy.marotta@yahoo.com>
2020-09-24 13:30:24 -0400

    Completed Implementation of makeTask in RandomTaskGenerator

--- src/test/java/edu/ithaca/dragon/par/pedagogicalModel/RandomTaskGeneratorTest.java
+++ src/test/java/edu/ithaca/dragon/par/pedagogicalModel/RandomTaskGeneratorTest.java
@@ -23,10 +23,10 @@ public class RandomTaskGeneratorTest {
         QuestionPool questionPool = new QuestionPool(new JsonQuestionPoolDatastore("src/test/resources/author/SampleQuestionPool.json").getAllQuestions());
         StudentModel studentModel = new StudentModel("TestUser1", questionPool.getAllQuestions());
         for (int i = 0; i < 50; i++){
-            ImageTask taskToCheck = randomTaskGenerator.makeTask(studentModel, 1);
+            ImageTask taskToCheck = randomTaskGenerator.makeTask(studentModel, 4);
             List<Question> questionsList = taskToCheck.getTaskQuestions();
-            for (int j = 0; j < questionsList.size(); i++){
-                System.out.println(questionsList.get(i).getType());
+            for (int j = 0; j < questionsList.size(); j++){
+                System.out.println(questionsList.get(j).getType());
             }
         }
     }
