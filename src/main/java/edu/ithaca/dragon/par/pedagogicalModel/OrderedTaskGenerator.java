7ef6ea18a3c34577c1022c32fba1a9f3bb4c7764
TimMarotta <timothy.marotta@yahoo.com>
2020-09-25 14:02:01 -0400

    Created Test for OrderedTaskGenerator

---
+++ src/main/java/edu/ithaca/dragon/par/pedagogicalModel/OrderedTaskGenerator.java
@@ -0,0 +1,11 @@
+package edu.ithaca.dragon.par.pedagogicalModel;
+
+import edu.ithaca.dragon.par.io.ImageTask;
+import edu.ithaca.dragon.par.studentModel.StudentModel;
+
+public class OrderedTaskGenerator implements TaskGenerator {
+    @Override
+    public ImageTask makeTask(StudentModel studentModel, int questionCountPerTypeForAnalysis) {
+        return null;
+    }
+}
---
+++ src/test/java/edu/ithaca/dragon/par/pedagogicalModel/OrderedTaskGeneratorTest.java
@@ -0,0 +1,38 @@
+package edu.ithaca.dragon.par.pedagogicalModel;
+
+import edu.ithaca.dragon.par.domainModel.Question;
+import edu.ithaca.dragon.par.domainModel.QuestionPool;
+import edu.ithaca.dragon.par.io.ImageTask;
+import edu.ithaca.dragon.par.io.JsonQuestionPoolDatastore;
+import edu.ithaca.dragon.par.studentModel.StudentModel;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.List;
+
+public class OrderedTaskGeneratorTest {
+
+    @Test
+    public void loopedQuestionSetTest() throws IOException {
+        OrderedTaskGenerator randomTaskGenerator = new OrderedTaskGenerator();
+        QuestionPool questionPool = new QuestionPool(new JsonQuestionPoolDatastore("src/test/resources/author/SampleQuestionPool.json").getAllQuestions());
+        StudentModel studentModel = new StudentModel("TestUser1", questionPool.getAllQuestions());
+        String firstQuestionType = null;
+        String lastQuestionType = null;
+        for (int i = 0; i < 10; i++){
+            ImageTask taskToCheck = randomTaskGenerator.makeTask(studentModel, 4);
+            List<Question> questionsList = taskToCheck.getTaskQuestions();
+            if (i == 0) {
+                firstQuestionType = questionsList.get(0).getType();
+            }
+        }
+
+        //ask for one more task to check that it loops
+        ImageTask taskToCheck = randomTaskGenerator.makeTask(studentModel, 4);
+        List<Question> questionsList = taskToCheck.getTaskQuestions();
+        lastQuestionType = questionsList.get(0).getType();
+
+        assertEquals(firstQuestionType, lastQuestionType);
+    }
+}
