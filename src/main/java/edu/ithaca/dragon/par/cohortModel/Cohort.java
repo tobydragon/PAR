2e1bec0b557a0eb7c19a984a33b1c3d2541aafa1
timothymarotta <tmarotta@ithaca.edu>
2020-10-05 12:08:08 -0400

    created Cohort class with getters/setters for TaskGenerator and list of Students

---
+++ src/main/java/edu/ithaca/dragon/par/cohortModel/Cohort.java
@@ -0,0 +1,32 @@
+package edu.ithaca.dragon.par.cohortModel;
+
+import edu.ithaca.dragon.par.pedagogicalModel.TaskGenerator;
+import edu.ithaca.dragon.par.studentModel.StudentModel;
+
+import java.util.List;
+
+public class Cohort {
+    private TaskGenerator taskGenerator;
+    private List<StudentModel> students;
+
+    public Cohort(TaskGenerator taskGenerator, List<StudentModel> students){
+        this.taskGenerator =  taskGenerator;
+        this.students = students;
+    }
+
+    public void setStudents(List<StudentModel> students) {
+        this.students = students;
+    }
+
+    public List<StudentModel> getStudents() {
+        return students;
+    }
+
+    public void setTaskGenerator(TaskGenerator taskGenerator) {
+        this.taskGenerator = taskGenerator;
+    }
+
+    public TaskGenerator getTaskGenerator() {
+        return taskGenerator;
+    }
+}
