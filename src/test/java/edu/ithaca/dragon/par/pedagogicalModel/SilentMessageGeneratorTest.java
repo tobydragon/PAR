5c8f7aa1ca52a0a2abc92511866bff57acfa1028
kerrybuckman <kbuckman@ithaca.edu>
2021-03-17 10:51:01 -0400

    more typos

--- src/test/java/edu/ithaca/dragon/par/pedagogicalModel/SilentMessageGeneratorTest.java
+++ src/test/java/edu/ithaca/dragon/par/pedagogicalModel/SilentMessageGeneratorTest.java
@@ -21,7 +21,7 @@ public class SilentMessageGeneratorTest {
 
         TaskGenerator taskGenerator = new LevelTaskGenerator(EquineQuestionTypes.makeLevelToTypesMap());
 
-        QuestionPool myQP = new QuestionPool(new JsonQuestionPoolDatastore("src/test/resources/author/questionPools/TestFullQP.json").getAllQuestions());
+        QuestionPool myQP = new QuestionPool(new JsonQuestionPoolDatastore("src/test/resources/author/questionPools/testFullQP.json").getAllQuestions());
         StudentModelRecord smr = JsonUtil.fromJsonFile("src/test/resources/author/students/masteredStudent.json", StudentModelRecord.class);
         StudentModel masteredStudentModel = smr.buildStudentModel(myQP);
         masteredStudentModel.setPreviousLevel(LevelTaskGenerator.calcLevel(masteredStudentModel.calcKnowledgeEstimateByType(4)));
@@ -33,7 +33,7 @@ public class SilentMessageGeneratorTest {
         assertEquals(null, message);
 
 
-        QuestionPool myQP2 = new QuestionPool(new JsonQuestionPoolDatastore("src/test/resources/author/questionPools/TestFullQP.json").getAllQuestions());
+        QuestionPool myQP2 = new QuestionPool(new JsonQuestionPoolDatastore("src/test/resources/author/questionPools/testFullQP.json").getAllQuestions());
 
         //repeated within 30 min, message
         StudentModelRecord  smr30 = JsonUtil.fromJsonFile("src/test/resources/author/students/buckmank.json", StudentModelRecord.class);
