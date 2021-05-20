1999dca18e3abdf6e164398136b8908437a2192a
Toby Dragon <dr.toby.dragon@gmail.com>
2021-05-20 07:02:31 -0400

    cohort plan

--- src/main/java/edu/ithaca/dragon/par/ParServer.java
+++ src/main/java/edu/ithaca/dragon/par/ParServer.java
@@ -7,6 +7,9 @@ import edu.ithaca.dragon.par.studentmodel2.StudentModelDatasource;
 
 public class ParServer {
 
+    //TODO: cohort needs to be kept here somehow, but complicated due to many-to-many relationship
+    //When we store a cohort, we will need to use special json syntax for the questionChooser:
+    // https://octoperf.com/blog/2018/02/01/polymorphism-with-jackson/#polymorphism
     private final DomainDatasource domainDatasource;
     private final StudentModelDatasource studentModelDatasource;
     private final QuestionChooser questionChooser;
