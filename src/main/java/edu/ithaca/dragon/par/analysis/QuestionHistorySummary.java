25cf1c032ca52be627b98c13f7d6ac28a9a55876
Toby Dragon <dr.toby.dragon@gmail.com>
2021-05-20 22:04:16 -0400

    planned future epics

---
+++ src/main/java/edu/ithaca/dragon/par/analysis/QuestionHistorySummary.java
@@ -0,0 +1,12 @@
+package edu.ithaca.dragon.par.analysis;
+
+import java.util.List;
+
+//TODO: can make these: across everyone for all types or certain type, for 1 student for all types or certain type, window size too
+public class QuestionHistorySummary {
+    private List<String> questionsSeen;
+    private List<String> questionsAnswered;
+    private List<String> questionsCorrectFirstTime;
+    private List<String> questionsCorrectAfterIncorrect;
+    private List<String> questionsOnlyAnsweredIncorrect;
+}
---
+++ src/main/java/edu/ithaca/dragon/par/pedagogy/QuestionChooserByOrderedConcepts.java
@@ -0,0 +1,15 @@
+package edu.ithaca.dragon.par.pedagogy;
+
+public class QuestionChooserByOrderedConcepts {
+    //Each concept(type) has an average of last <window-size> questions which gets bucketed
+    //rubric-style: unprepared,developing,competent,exemplary
+    //having too few questions answered puts you automatically into developing
+    //concepts are ordered
+    //unprepared on next topic can get turned to developing by competent grade on prior
+    //exemplary on the prior topic can get turned into competent by current topic
+    //only ask questions on topics that are developing or competent
+
+    //Find questions of the right type(s) that have the min seen count.
+    //If a minSeen question has the same URL as last question, take that, otherwise go for first minSeen
+
+}
