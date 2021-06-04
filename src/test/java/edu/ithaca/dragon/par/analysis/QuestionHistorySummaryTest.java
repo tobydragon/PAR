1c5013b0f52a97ff74887d9855bde82a9234db7f
vkumar825 <vkumar825@users.noreply.github.com>
2021-06-04 16:01:52 -0400

    wrote tests for QuestionHistorySummary

--- src/test/java/edu/ithaca/dragon/par/analysis/QuestionHistorySummaryTest.java
+++ src/test/java/edu/ithaca/dragon/par/analysis/QuestionHistorySummaryTest.java
@@ -19,8 +19,17 @@ class QuestionHistorySummaryTest {
     @Test
     public void buildQuestionIdsSeenTest(){
         List<String> questionIdsSeen = QuestionHistorySummary.buildQuestionIdsSeen(QuestionHistoryTest.makeExamples());
-        assertEquals(1, questionIdsSeen.size());
+        assertEquals(3, questionIdsSeen.size());
         assertEquals("q1", questionIdsSeen.get(0));
+        assertEquals("q2", questionIdsSeen.get(1));
+        assertEquals("q3", questionIdsSeen.get(1));
+    }
+
+    @Test
+    public void buildQuestionsCorrectFirstTime(){
+        List<String> questionsCorrectFirstTime = QuestionHistorySummary.buildQuestionsCorrectFirstTime(QuestionHistoryTest.makeExamples());
+        assertEquals(1, questionsCorrectFirstTime.size());
+        assertEquals("q2", questionsCorrectFirstTime.get(0));
     }
 
 }
