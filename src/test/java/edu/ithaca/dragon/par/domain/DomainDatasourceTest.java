5bb3efd0f68793333f20d593318ac78f10bd55e7
vkumar825 <vkumar825@users.noreply.github.com>
2021-06-08 07:09:23 -0400

    more tests for gettersTest

--- src/test/java/edu/ithaca/dragon/par/domain/DomainDatasourceTest.java
+++ src/test/java/edu/ithaca/dragon/par/domain/DomainDatasourceTest.java
@@ -13,8 +13,17 @@ class DomainDatasourceTest {
     void gettersTest() throws IOException {
         DomainDatasource domainDatasource = new DomainDatasourceSimple(JsonUtil.listFromJsonFile("src/test/resources/rewrite/SampleQuestions.json", Question.class));
         assertEquals(3, domainDatasource.getAllQuestions().size());
+
+        assertEquals("blue", domainDatasource.getQuestion("generic0").getCorrectAnswer());
+        assertEquals("What is the color of the sky?", domainDatasource.getQuestion("generic0").getQuestionText());
+
         assertEquals("2", domainDatasource.getQuestion("generic1").getCorrectAnswer());
+        assertEquals("What is 1 + 1?", domainDatasource.getQuestion("generic1").getQuestionText());
+
+        assertEquals("CS", domainDatasource.getQuestion("generic2").getCorrectAnswer());
+        assertEquals("What is your major?", domainDatasource.getQuestion("generic2").getQuestionText());
 
         assertThrows(IllegalArgumentException.class, ()-> domainDatasource.getQuestion("invalidId"));
     }
+
 }
