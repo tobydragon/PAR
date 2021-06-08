325feca8794442db72b55c89bed52e1e6b3e682a
vkumar825 <vkumar825@users.noreply.github.com>
2021-06-07 20:16:19 -0400

    changed map to use list

--- src/test/java/edu/ithaca/dragon/par/student/json/QuestionHistoryTest.java
+++ src/test/java/edu/ithaca/dragon/par/student/json/QuestionHistoryTest.java
@@ -2,41 +2,46 @@ package edu.ithaca.dragon.par.student.json;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class QuestionHistoryTest {
 
-    public static Map<String, QuestionHistory> makeExamples(){
-        Map<String, QuestionHistory> examples = new HashMap<>();
+    public static List<QuestionHistory> makeExamples(){
+        //TODO: change Map to use List
+        
+        //Map<String, QuestionHistory> examples = new HashMap<>();
+        List<QuestionHistory> examples = new ArrayList<>();
 
         QuestionHistory ex1 = new QuestionHistory("q1");
         ex1.addTimeSeen();
         ex1.addTimeSeen();
         ex1.addResponse("a");
         ex1.addResponse("b");
-        examples.put(ex1.getQuestionId(), ex1);
+        examples.add(ex1);
 
         QuestionHistory ex2 = new QuestionHistory("q2");
         ex2.addTimeSeen();
         ex2.addTimeSeen();
-        examples.put(ex2.getQuestionId(), ex2);
+        examples.add(ex2);
 
         QuestionHistory ex3 = new QuestionHistory("q3");
         ex3.addTimeSeen();
         ex3.addResponse("c");
-        examples.put(ex3.getQuestionId(), ex3);
+        examples.add(ex3);
 
         QuestionHistory ex4 = new QuestionHistory("q4");
         ex4.addTimeSeen();
-        examples.put(ex4.getQuestionId(), ex4);
+        examples.add(ex4);
 
         QuestionHistory ex5 = new QuestionHistory("q5");
         ex5.addTimeSeen();
         ex5.addTimeSeen();
         ex5.addResponse("d");
         ex5.addResponse("e");
-        examples.put(ex5.getQuestionId(), ex5);
+        examples.add(ex5);
 
         return examples;
     }
