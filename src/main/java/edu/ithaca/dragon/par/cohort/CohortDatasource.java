9e5dcebb9b676bf9cdb899ee07d3091407bf8065
vkumar825 <vkumar825@users.noreply.github.com>
2021-06-10 13:56:16 -0400

    defined getCohortIds in the interface

--- src/main/java/edu/ithaca/dragon/par/cohort/CohortDatasource.java
+++ src/main/java/edu/ithaca/dragon/par/cohort/CohortDatasource.java
@@ -1,7 +1,10 @@
 package edu.ithaca.dragon.par.cohort;
 
+import java.util.List;
+
 import edu.ithaca.dragon.par.pedagogy.QuestionChooser;
 
 public interface CohortDatasource {
     public QuestionChooser getQuestionChooser(String studentId);
+    public List<String> getCohortIds();
 }
