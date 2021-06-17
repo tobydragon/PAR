4cc1f071b42cfb377f96d9aba2065f55fa3ffd9d
pseudaimonia <57679024+pseudaimonia@users.noreply.github.com>
2021-06-17 17:27:27 -0400

    QuestionChooserTest start w/ sample question histories formatting example

--- src/test/java/edu/ithaca/dragon/par/pedagogy/QuestionChooserByOrderedConceptsTest.java
+++ src/test/java/edu/ithaca/dragon/par/pedagogy/QuestionChooserByOrderedConceptsTest.java
@@ -1,6 +1,8 @@
 package edu.ithaca.dragon.par.pedagogy;
 
 import edu.ithaca.dragon.par.domain.DomainDatasource;
+import edu.ithaca.dragon.par.student.StudentModelInfo;
+import edu.ithaca.dragon.par.student.json.StudentModelJson;
 import edu.ithaca.dragon.par.domain.DomainDatasourceSimple;
 import edu.ithaca.dragon.par.domain.Question;
 import edu.ithaca.dragon.util.JsonUtil;
@@ -10,6 +12,18 @@ import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class QuestionChooserByOrderedConceptsTest {
-    
+class QuestionChooserByOrderedConceptsTest{
+
+    //level 1: sky
+    //level 2: math
+    //level 3: major
+    //level 4: year
+    //level 5: google
+
+    public void chooseQuestionTest() throws IOException{
+    DomainDatasource domainDatasource = new DomainDatasourceSimple(JsonUtil.listFromJsonFile("src/test/resources/rewrite/SampleQuestions.json", Question.class));
+    StudentModelInfo studentModel = new StudentModelJson();
+    QuestionChooserByOrderedConcepts questionChooser = new QuestionChooserByOrderedConcepts();
+    // assertEquals("generic0",questionChooser.chooseQuestion(StudentModelInfo.class,domainDatasource));
+    }
 }
