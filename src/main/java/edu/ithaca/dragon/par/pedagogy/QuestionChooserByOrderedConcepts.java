36cf504aa8d85dd52dda9fd3dfb676a6dfc3dbef
pseudaimonia <57679024+pseudaimonia@users.noreply.github.com>
2021-06-22 17:05:41 -0400

    question chooser test cases w/ sample data

--- src/main/java/edu/ithaca/dragon/par/pedagogy/QuestionChooserByOrderedConcepts.java
+++ src/main/java/edu/ithaca/dragon/par/pedagogy/QuestionChooserByOrderedConcepts.java
@@ -1,5 +1,13 @@
 package edu.ithaca.dragon.par.pedagogy;
 
+import java.util.List;
+
+import edu.ithaca.dragon.par.domain.DomainDatasource;
+import edu.ithaca.dragon.par.domain.Question;
+import edu.ithaca.dragon.par.student.StudentModelInfo;
+
+
+
 public class QuestionChooserByOrderedConcepts {
     //Each concept(type) has an average of last <window-size> questions which gets bucketed
     //rubric-style: unprepared,developing,competent,exemplary
@@ -12,4 +20,21 @@ public class QuestionChooserByOrderedConcepts {
     //Find questions of the right type(s) that have the min seen count.
     //If a minSeen question has the same URL as last question, take that, otherwise go for first minSeen
 
+    public QuestionChooserByOrderedConcepts(){}
+
+    public Question chooseQuestion(StudentModelInfo studentModelInfo, DomainDatasource domainDatasource){
+        //Algorithm: 
+        // 1. get all questions from domain datasource
+        // 2. select the first concept from the list of questions to be developing, the rest are unprepared
+        // 3. (Once competent, the next concept will be developing)
+        // 4. (Once exemplary, the next concept will be updated to competent)
+        // 5. from the developing and competent buckets, the question asked will be chosen based on least recently seen
+        // *Test case for when first two topics are exemplary and the next 
+        return new Question();
+    }
+
+    public List<Question> calcUnprepared(StudentModelInfo studentModelInfo, DomainDatasource domainDatasource){}
+    public List<Question> calcDeveloping(StudentModelInfo studentModelInfo, DomainDatasource domainDatasource){}
+    public List<Question> calcCompetent(StudentModelInfo studentModelInfo, DomainDatasource domainDatasource){}
+    public List<Question> calcExemplary(StudentModelInfo studentModelInfo, DomainDatasource domainDatasource){}
 }
--- src/test/java/edu/ithaca/dragon/par/pedagogy/QuestionChooserByOrderedConceptsTest.java
+++ src/test/java/edu/ithaca/dragon/par/pedagogy/QuestionChooserByOrderedConceptsTest.java
@@ -1,10 +1,13 @@
 package edu.ithaca.dragon.par.pedagogy;
 
 import edu.ithaca.dragon.par.domain.DomainDatasource;
+import edu.ithaca.dragon.par.student.StudentModelDatasource;
 import edu.ithaca.dragon.par.student.StudentModelInfo;
+import edu.ithaca.dragon.par.student.json.StudentModelDatasourceJson;
 import edu.ithaca.dragon.par.student.json.StudentModelJson;
 import edu.ithaca.dragon.par.domain.DomainDatasourceSimple;
 import edu.ithaca.dragon.par.domain.Question;
+import edu.ithaca.dragon.util.JsonIoHelperDefault;
 import edu.ithaca.dragon.util.JsonUtil;
 import org.junit.jupiter.api.Test;
 
@@ -19,11 +22,50 @@ class QuestionChooserByOrderedConceptsTest{
     //level 3: major
     //level 4: year
     //level 5: google
-
+    @Test
     public void chooseQuestionTest() throws IOException{
-    DomainDatasource domainDatasource = new DomainDatasourceSimple(JsonUtil.listFromJsonFile("src/test/resources/rewrite/SampleQuestions.json", Question.class));
-    StudentModelInfo studentModel = new StudentModelJson();
+    //new Student test
+    DomainDatasource domainDatasource = new DomainDatasourceSimple(JsonUtil.listFromJsonFile("src/test/resources/rewrite/QuestionChooserSampleQuestions.json", Question.class));
+    StudentModelDatasourceJson studentModelDatasource = new StudentModelDatasourceJson("chooserExample", "src/test/resources/rewrite/questionChooserSampleStudents", new JsonIoHelperDefault());
+    StudentModelJson newStudentModel = studentModelDatasource.getStudentModel("newStudent");
     QuestionChooserByOrderedConcepts questionChooser = new QuestionChooserByOrderedConcepts();
-    // assertEquals("generic0",questionChooser.chooseQuestion(StudentModelInfo.class,domainDatasource));
+    assertEquals("skyQ1",questionChooser.chooseQuestion(newStudentModel,domainDatasource).getId());
+
+    assertEquals("skyQ1",questionChooser.calcDeveloping(newStudentModel, domainDatasource).get(0).getId());
+    assertEquals("skyQ2",questionChooser.calcDeveloping(newStudentModel, domainDatasource).get(1).getId());
+    assertEquals("skyQ3",questionChooser.calcDeveloping(newStudentModel, domainDatasource).get(2).getId());
+
+
+    //first concept competent test
+
+    StudentModelJson studentModelFirstConceptCompetent = studentModelDatasource.getStudentModel("firstConceptCompetentStudent");
+    
+    assertEquals("skyQ1",questionChooser.calcCompetent(studentModelFirstConceptCompetent, domainDatasource).get(0).getId());
+    assertEquals("skyQ2",questionChooser.calcCompetent(studentModelFirstConceptCompetent, domainDatasource).get(1).getId());
+    assertEquals("skyQ3",questionChooser.calcCompetent(studentModelFirstConceptCompetent, domainDatasource).get(2).getId());
+
+    assertEquals("mathQ1",questionChooser.chooseQuestion( studentModelFirstConceptCompetent,domainDatasource).getId());
+
+    assertEquals("mathQ1",questionChooser.calcDeveloping(studentModelFirstConceptCompetent, domainDatasource).get(0).getId());
+    assertEquals("mathQ2",questionChooser.calcDeveloping(studentModelFirstConceptCompetent, domainDatasource).get(1).getId());
+
+    //first concept exemplary test
+
+    StudentModelJson studentModelFirstConceptExemplary = studentModelDatasource.getStudentModel("firstConceptCompetentStudent");
+
+    assertEquals("skyQ1",questionChooser.calcExemplary(studentModelFirstConceptExemplary, domainDatasource).get(0).getId());
+    assertEquals("skyQ2",questionChooser.calcExemplary(studentModelFirstConceptExemplary, domainDatasource).get(1).getId());
+    assertEquals("skyQ3",questionChooser.calcExemplary(studentModelFirstConceptExemplary, domainDatasource).get(2).getId());
+
+    assertEquals("mathQ1",questionChooser.chooseQuestion( studentModelFirstConceptExemplary,domainDatasource).getId());
+
+    assertEquals("mathQ1",questionChooser.calcCompetent(studentModelFirstConceptExemplary, domainDatasource).get(0).getId());
+    assertEquals("mathQ2",questionChooser.calcCompetent(studentModelFirstConceptExemplary, domainDatasource).get(1).getId());
+    
+    assertEquals("majorQ1",questionChooser.calcDeveloping(studentModelFirstConceptExemplary, domainDatasource).get(0).getId());
+    assertEquals("majorQ2",questionChooser.calcDeveloping(studentModelFirstConceptExemplary, domainDatasource).get(1).getId());
+
+    
+
     }
 }
